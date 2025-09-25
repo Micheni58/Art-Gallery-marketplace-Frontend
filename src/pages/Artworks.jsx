8d06c6ce6fa10db54072c712b670a5f95a65e2dd
Micheni58 <michenicaleb@gmail.com>
2025-09-25 12:40:30 +0300

    Add styling to artworks and navbar

--- src/pages/Artworks.jsx
+++ src/pages/Artworks.jsx
@@ -1,16 +1,31 @@
-// This page is showing the list of artworks in the db
-import ArtworkCard from "../Components/ArtworkCard";
-function Artworks(){
-    return (
-        <>
-        <div className="p-4 grid ">
-            <h1 className="text-3xl">Artworks Page</h1>
-            <p>Explore our curated collection 
-            of exceptional artworks from talented artists around the world.
-            </p>
-            <ArtworkCard />
-        </div>
-        </>
-    )
+import { useEffect, useState } from "react"
+import ArtworkCard from "../Components/ArtworkCard"
+
+function Artworks() {
+  const [artworks, setArtworks] = useState([])
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/artworks")
+      .then((res) => res.json())
+      .then((data) => setArtworks(data))
+      .catch((err) => console.error("Error fetching artworks:", err))
+  }, [])
+
+  return (
+    <div className="p-4 min-h-screen bg-gradient-to-r from-purple-300 via-purple-200 to-blue-200">
+      <h1 className="text-3xl mb-4">Artworks Page</h1>
+      <p className="mb-6">
+        Explore our curated collection of exceptional artworks from talented
+        artists around the world.
+      </p>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {artworks.map((art) => (
+          <ArtworkCard key={art.id} art={art} />
+        ))}
+      </div>
+    </div>
+  )
 }
-export default Artworks;
+
+export default Artworks
