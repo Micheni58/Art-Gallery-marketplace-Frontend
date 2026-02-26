8240de24822d952e9271fd46e0523786efcd517e
monica <monica.njoki@student.moringaschool.com>
2026-02-26 12:09:00 +0300

    Wrap App with BrowserRouter to fix refresh 404

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; //  import BrowserRouter
 import "./index.css";
 import App from "./App.jsx";
-import { AuthProvider } from "./context/AuthContext.jsx"; // ✅ import provider
+import { AuthProvider } from "./context/AuthContext.jsx"; //  import provider
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <BrowserRouter basename="/">   {/* wrap App with BrowserRouter */}
+        <App />
+      </BrowserRouter>
     </AuthProvider>
   </StrictMode>
 );
